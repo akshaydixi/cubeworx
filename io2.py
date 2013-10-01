64a5afc92af1be20533d96337cdd8c5e17b8f87a
Akshay Dixit <akshaydixi@gmail.com>
2013-10-01 12:55:15 +0530

    fix

--- io2.py
+++ io2.py
@@ -5,9 +5,11 @@ camera = cv2.VideoCapture(0)
 
 while True:
     ret,frame = camera.read()
-    gray = cv2.cvtColor(frame,cv2.COLOT_BGR2GRAY)
+    if ret:
+        frame = cv2.flip(frame,1)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame',gray)
-    if cv2.waitKey(0) & 0xFF == 27:
+    if cv2.waitKey(1) & 0xFF == 27:
         break
 
 camera.release()
